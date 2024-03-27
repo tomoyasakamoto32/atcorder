0bbb209621e3b804de085988110745a70f4420a1
坂本 友哉sakamoto tomoya <oregairu0302@gmail.com>
2024-03-27 09:58:30 +0900

    atcoder practice

--- abc346/src/bin/b.rs
+++ abc346/src/bin/b.rs
@@ -5,9 +5,27 @@ fn main() {
         w: i32,
         b: i32,
     }
+    let mut t = "wbwbwwbwbwbw".to_string();
+    for _ in 0..100 {
+        t += "wbwbwwbwbwbw"
+    }
+    for i in 0..12 {
+        let mut nw = 0;
+        let mut nb = 0;
 
-    println!("{}, {}", w, b);
-    let a = 36 / 52;
-    let c = b / w;
-    if a > c {}
+        for j in 0..w + b {
+            let index = i + j;
+            let a = &t[(index - 1) as usize..index as usize];
+            if a == "w" {
+                nw += 1
+            } else {
+                nb += 1
+            }
+        }
+        if nw == w && nb == b {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
