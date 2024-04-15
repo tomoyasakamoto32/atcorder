ce80c0509a0df7421ffc06994d9bb70f651229a1
坂本 友哉sakamoto tomoya <oregairu0302@gmail.com>
2024-04-15 10:37:17 +0900

    atcoder practice

--- abc349/src/bin/c.rs
+++ abc349/src/bin/c.rs
@@ -1,36 +1,24 @@
 use proconio::input;
-use std::collections::HashMap;
 
 fn main() {
     input! {
-        s: String,
+        mut s: String,
         t: String,
-    }
-
-    let mut scores: HashMap<char, i32> = HashMap::new();
-    for t in s.chars() {
-        let a = scores.get(&t);
-        if let Some(v) = a {
-            scores.insert(t, *v + 1);
-        } else {
-            scores.insert(t, 1);
-        }
     }
 
     let mut flag = true;
     for (index, t_item) in t.chars().enumerate() {
+        let ind = s.chars().position(|r| r == t_item.to_ascii_lowercase());
         if index + 1 == 3 && t_item == 'X' {
             break;
         }
-        let c = t_item.clone().to_ascii_lowercase();
-        let q = scores.get(&c);
-        if let Some(v) = q {
-            if v.clone() <= 0 {
-                flag = false;
-            }
-            scores.insert(c, *v - 1);
+        if let Some(x) = ind {
+            let current = &s[x + 1..].to_string();
+            let b = current.to_string().clone();
+            s = b;
         } else {
             flag = false;
+            break;
         }
     }
 
