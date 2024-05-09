e30f4f5a00ab903afb61a25d8f07b25d4e13a8ca
坂本 友哉sakamoto tomoya <oregairu0302@gmail.com>
2024-05-09 10:33:28 +0900

    atcoder practice

--- abc351/src/bin/a.rs
+++ abc351/src/bin/a.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: [i32; 9],
+        b: [i32; 8],
+    }
+
+    let mut total_a = 0;
+
+    for a_item in a {
+        total_a += a_item;
+    }
+
+    let mut total_b = 0;
+
+    for b_item in b {
+        total_b += b_item;
+    }
+
+    println!("{}", total_a - total_b + 1);
 }
